e9bffb24ca9c54465ff2f3eb312fbe6f8743b6ac
Dawid Jeż <dawidjez@gmail.com>
2024-12-27 21:15:45 +0100

    Refactor StartScreen with modular components
    Extracted `titleText` and `startGameBtn` into separate reusable components to enhance code modularity and clarity. This improves the organization and maintainability of the `StartScreen` layout and simplifies future updates or reuse.

---
+++ lib/Screens/StartScreen/VisualComponents/title_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const titleText = Text(
+  'Circle Jump',
+  style: TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: Colors.blue,
+  ),
+);
--- lib/Screens/StartScreen/start_screen.dart
+++ lib/Screens/StartScreen/start_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'VisualComponents/start_game_btn.dart';
+import 'VisualComponents/title_text.dart';
+
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
@@ -21,32 +24,9 @@ class StartScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Circle Jump',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
+              titleText,
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/game');
-                },
-                style: ElevatedButton.styleFrom(
-                  padding:
-                  const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                  backgroundColor: Colors.blue,
-                ),
-                child: const Text(
-                  'Start Game',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+              startGameBtn(context),
             ],
           ),
         ),
