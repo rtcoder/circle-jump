70ba2760985e5dd4b6b2a0194e1eb495f5b37b57
Dawid Jeż <dawidjez@gmail.com>
2025-01-04 16:30:33 +0100

    Update height_on_platform.dart file path
    Moved height_on_platform.dart to the Platform folder for improved organization. This change aligns related files and simplifies project structure for better maintainability.

--- lib/Models/player_platform.dart
+++ lib/Models/player_platform.dart
@@ -1,6 +1,6 @@
 import 'package:circle_jump/Models/Platform/platform.dart';
 import 'package:circle_jump/Models/game.dart';
-import 'package:circle_jump/Models/height_on_platform.dart';
+import 'package:circle_jump/Models/Platform/height_on_platform.dart';
 
 import '../Enums/platform_type.dart';
 
