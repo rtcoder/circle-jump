a3596ecf9bba240e58528a59b2a47e4901721752
Dawid Jeż <dawidjez@gmail.com>
2025-01-10 18:42:35 +0100

    Set app orientation to landscape mode.
    Ensured landscape orientation is enforced on Android and iOS by updating AndroidManifest.xml, Info.plist, and main.dart. Adjustments include removal of portrait support and explicit orientation locking during app initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:circle_jump/circle_jump.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const CircleJump());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
+    runApp(const CircleJump());
+  });
 }
