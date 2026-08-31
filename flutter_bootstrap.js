cbca560242da3f38432ddccc3cb5cf2d0193fedd
rtcoder <rtcoder@users.noreply.github.com>
2026-08-31 18:07:57 +0000

    deploy: e060eb2299f40cf050b7917d2858ebd25513d41d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "128808033"
+    serviceWorkerVersion: "3236240493"
   }
 });
